2da591992bba5fbc4d99c204d5feacfc194596d8
Samya050804 <141639191+Samya050804@users.noreply.github.com>
2023-08-14 07:52:41 -0700

    Create Java Program to Find the Middle Element of an Array By Dynamic Initialization of Array Elements

---
+++ Java Program to Find the Middle Element of an Array By Dynamic Initialization of Array Elements.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter the number of elements in the array: "); 
+        int num = sc.nextInt(); 
+        int arr[] = new int[num]; 
+        System.out.print("Enter the elements: "); 
+        for (int i = 0; i < num; i++) 
+        { 
+            arr[i] = sc.nextInt(); 
+        }
+        
+        // Printing the array
+        System.out.println("The array elements are : "+Arrays.toString(arr));
+        int startIndex = 0, lastIndex = arr.length - 1;
+        
+        int midIndex = startIndex + (lastIndex-startIndex)/2;
+        System.out.println("The mid index is "+midIndex+" and the element at mid is "+arr[midIndex]);
+    }
+}
